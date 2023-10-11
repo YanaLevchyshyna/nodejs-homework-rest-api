6180343362baed7b55188e0657c99986df63d32c
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-10-11 23:29:05 +0300

    first commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,110 @@
-const express = require('express')
+import express from 'express';
+import Joi from 'joi';
+import * as contactsService from '../../models/contacts.js';
 
-const router = express.Router()
+import { HttpError } from '../../helpers/index.js';
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactsRouter = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// створюємо обʼєкт в якому зазначаємо, які саме властивості мають бути обовʼязковими у тілі реквеста
+const contactAddSchema = Joi.object({
+  id: Joi.any(),
+  name: Joi.string().required().messages({
+    'any.required': `missing required field "title"`,
+  }),
+  email: Joi.string().required().messages({
+    'any.required': `missing required field "email"`,
+  }),
+  phone: Joi.number().required().messages({
+    'any.required': `missing required field "phone"`,
+  }),
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get('/', async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// якщо виникає помилка в середині try, вона потрапляє в catch.
+// catch бере помилку і передає некст.
+// Некст бере помилку і починає перебирати всі
+// колбеки та шукає той в якого 4 параметри.
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-module.exports = router
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.post('/', async (req, res, next) => {
+  try {
+    // Перевіряємо чи тіло реквеста не пусте
+    if (!Object.keys(req.body).length) {
+      throw new Error(400, 'missing required name field');
+    }
+    // перед додаванням, перевіряємо на наявність обовʼязкових параметрів у тілі запиту відповідно до схеми
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json({ message: 'contact deleted' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+contactsRouter.put('/:id', async (req, res, next) => {
+  try {
+    // Перевіряємо чи тіло реквеста не пусте
+    if (!Object.keys(req.body).length) {
+      throw new Error(400, 'All fields are empty');
+    }
+
+    // перед додаванням, перевіряємо на наявність обовʼязкових параметрів у тілі запиту відповідно до схеми
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, 'missing fields');
+    }
+
+    const { id } = req.params;
+    const result = await contactsService.updateContactById(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    } // якщо повернеться null, то викидуємо 404 помилку
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default contactsRouter;
